c0452c9b462eb7c477bbd4d2d75f0a311d544291
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-07-19 10:10:48 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -475,7 +475,7 @@ if user_input_excel is not None:
             # df_remaining_ltl = df_ltl_only.drop(df_ltl_only_winner_row).reset_index(drop=True)
             # st.write(df_remaining_ltl)
             
-            df_remaining_participant = pd.concat([df_remaining_ltl, df_remaining_non_ltl]).reset_index(drop=True)
+            df_remaining_participant = pd.concat(df_remaining_ltl, df_remaining_non_ltl).reset_index(drop=True)
             st.write(df_remaining_participant)
 
             # random.seed(st.session_state.user_input_seed+1)
