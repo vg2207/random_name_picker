f611bcc631ae8f7a9a6376818cfa5da697ffb1e9
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-07-19 10:12:33 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -463,7 +463,7 @@ if user_input_excel is not None:
             st.write(df_all_participant)
             # winners_name_all = []
 
-            df_ltl_only = df_all_participant.loc[df_all_participant['Company'] == 'LTL'].reset_index(drop=True)
+            df_ltl_only = df_all_participant.loc[df_all_participant['Company'] == 'LTL']
             # st.write(df_ltl_only)
             df_remaining_non_ltl = df_all_participant.loc[df_all_participant['Company'] != 'LTL'].reset_index(drop=True)
             # st.write(df_remaining_non_ltl)
