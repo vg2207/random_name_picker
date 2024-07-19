aebb8a266d36013117dd55db6d0742f9f4493982
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-07-19 17:34:03 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -383,20 +383,20 @@ if user_input_excel is not None:
         # if button_clicked_5 :
         new_row = pd.DataFrame(
             {"Number of Winner(s)": [90, 100,
-                                     14, 10,
-                                     20, 5,
-                                     1, 1,
-                                     1, 75,
+                                     # 14, 10,
+                                     # 20, 5,
+                                     # 1, 1,
+                                     # 1, 75,
                                      50, 30,
                                      5, 3,
                                      3, 1,
                                      1
                                     ],
              "Prize": ['Goodiebag Fibercreme dan Rich Creme', 'Voucher Indomaret Rp 50.000', 
-                       'Hydrogen Tumbler', 'Massage Pillow',
-                       'Blender Juicer', 'Free Scaling Voucher - Audy Dental',
-                       'Lock n lock container', 'Cosmos Mixer',
-                       'One Set Oxone Knife', 'Voucher Indomaret Rp 100.000',
+                       # 'Hydrogen Tumbler', 'Massage Pillow',
+                       # 'Blender Juicer', 'Free Scaling Voucher - Audy Dental',
+                       # 'Lock n lock container', 'Cosmos Mixer',
+                       # 'One Set Oxone Knife', 'Voucher Indomaret Rp 100.000',
                        'Voucher Indomaret Rp 150.000', 'Voucher product pureve: Rp 500.000',
                        'Xiaomi Smart Band 8', 'Traveloka voucher Rp 500.000',
                        'Garmin Watch (by GE)', 'Smart TV 32 inch Xiaomi',
@@ -505,7 +505,7 @@ if user_input_excel is not None:
             output = BytesIO()
 
             with pd.ExcelWriter(output, engine='xlsxwriter') as writer: 
-                for i in range(10) :
+                for i in range(9) :
                     # sheetname = 'Round ' + str(int(i+1)) + ' - Winner ' + str(df2.loc[i, 'Prize'])
                     sheetname = 'Round ' + str(int(i+1))
                     winners_data = winners_name_all[i].reset_index(drop=True)
