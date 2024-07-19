4beeab0b0e4d6d6d5dc8689120c7976b129e952b
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-07-19 16:37:06 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -469,7 +469,7 @@ if user_input_excel is not None:
             # st.write(df_remaining_non_ltl)
             
             random.seed(st.session_state.user_input_seed)
-            df_ltl_only_winner_row = random.sample(range(len(df_ltl_only)),3)
+            df_ltl_only_winner_row = random.sample(range(len(df_ltl_only)), 190)
             df_ltl_only_winner = df_ltl_only.iloc[df_ltl_only_winner_row].reset_index(drop=True)
             # st.write(df_ltl_only_winner)
             df_remaining_ltl = df_ltl_only.drop(df_ltl_only_winner_row).reset_index(drop=True)
@@ -481,7 +481,7 @@ if user_input_excel is not None:
 
             random.seed(st.session_state.user_input_seed)
             for i in range(len(df2)):
-                if i == 0 or i == 2 :
+                if i == 0 or i == 1 or i == 6 :
                     winners_row = random.sample(range(len(df_remaining_participant)),int(df2["Number of Winner(s)"][i]))
                     winners_name = df_remaining_participant.iloc[winners_row]
                     prize = df2["Prize"][i]
