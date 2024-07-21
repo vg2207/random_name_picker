c890a66658b0952ddd929e5d26905a3fed1a1812
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-07-21 15:40:29 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -293,6 +293,7 @@ if user_input_excel is not None:
     
                     row_number_to_show = st.session_state.count - 1
                     data_to_show = winners_name_all[row_number_to_show].reset_index(drop=True)
+                    data_to_show.rename(columns = {"NIK (tuliskan dengan lengkap dan kapital, contoh: LTL002965)":"NIK"}, inplace = True)
                     # data_to_show = data_to_show.columns.str.replace("NIK (tuliskan dengan lengkap dan kapital, contoh: LTL002965)", "NIK")
                     data_to_show.index += 1
                     # prize_to_choose = st.session_state.df2.copy()
@@ -300,7 +301,7 @@ if user_input_excel is not None:
                     # st.markdown(f"<h1 style='text-align: center;'>Pemenang Hadiah berupa {str(prize_to_show)} adalah</h1>", unsafe_allow_html=True)
 
                     with st.container():
-                        st.dataframe(data_to_show[["NIK (tuliskan dengan lengkap dan kapital, contoh: LTL002965)", "Nama Lengkap","Company"]], use_container_width=True)
+                        st.dataframe(data_to_show[["NIK", "Nama Lengkap","Company"]], use_container_width=True)
 
                     # for m in range(len(data_to_show)) :
                     #     st.text_area("", 
