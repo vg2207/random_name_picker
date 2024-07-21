3acb5e827ecc63f77629237c340ed5af21b7aa15
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-07-21 15:27:22 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -268,16 +268,21 @@ if user_input_excel is not None:
                     # st.markdown(f"<h2 style='text-align: center;'>Pemenang Hadiah berupa </h2>", unsafe_allow_html=True)
                     st.markdown(f"<h1 style='text-align: center; color: #0349b3;'>{str(prize_to_show)}</h1>", unsafe_allow_html=True)
                     time.sleep(1)
-                    with st.empty():
-                        while t :
-                            if t > 0 :
-                                with st.empty():
-                                    name_show = df.iloc[random.randint(1, len(df)) - 1]
-                                    
-                                    st.markdown(f"<h1 style='text-align: center;'><br>{str(name_show['Nama Lengkap'])}</h1>", unsafe_allow_html=True)
-                                time.sleep(0.1)
-                                t -= 1
-                                st.empty()
+                    if st.session_state.count <= 12 :
+                        with st.empty():
+                            while t :
+                                if t > 0 :
+                                    with st.empty():
+                                        name_show = df.iloc[random.randint(1, len(df)) - 1]
+                                        
+                                        st.markdown(f"<h1 style='text-align: center;'><br>{str(name_show['Nama Lengkap'])}</h1>", unsafe_allow_html=True)
+                                    time.sleep(0.1)
+                                    t -= 1
+                                    st.empty()
+                    else :
+                        with st.empty():
+                            st.markdown(f"<h1 style='text-align: center;'><br>{str(name_show['Nama Lengkap'])}</h1>", unsafe_allow_html=True)
+    
                     row_number_to_show = st.session_state.count - 1
                     data_to_show = winners_name_all[row_number_to_show].reset_index(drop=True)
                     # data_to_show = data_to_show.columns.str.replace("NIK (tuliskan dengan lengkap dan kapital, contoh: LTL002965)", "NIK")
