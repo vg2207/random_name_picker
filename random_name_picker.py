20185b1449d719c11949e13a7ef4e34844ad1990
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-07-24 09:51:59 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -25,18 +25,20 @@ def remove_duplicate(data, drop_col) :
 
 st.set_page_config(layout="wide")
 
-col_A, col_B, col_C = st.columns([1, 3, 1])
+# col_A, col_B, col_C = st.columns([1, 3, 1])
 
-with col_A :
-    logo = Image.open('assets_logo/Logo Lautan Luas.png')
-    st.image(logo)
-with col_B:
-    st.markdown(f"<h1 style='text-align: center;'>Undian Ulang Tahun ke 73<br>PT Lautan Luas Tbk.</h1>", unsafe_allow_html=True)
-with col_C :
-    logo_a = Image.open('assets_logo/anniversary_logo.png')
-    st.image(logo_a)
+# with col_A :
+#     logo = Image.open('assets_logo/Logo Lautan Luas.png')
+#     st.image(logo)
+# with col_B:
+#     st.markdown(f"<h1 style='text-align: center;'>Undian Ulang Tahun ke 73<br>PT Lautan Luas Tbk.</h1>", unsafe_allow_html=True)
+# with col_C :
+#     logo_a = Image.open('assets_logo/anniversary_logo.png')
+#     st.image(logo_a)
         
-    
+logo = Image.open('assets_logo/LOGO_FIXED.png')
+with st.container():
+    st.image(logo)
 
 
 
