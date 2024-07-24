3edb67a0093b88518ada2b8bda2091d2c175bd5d
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-07-24 09:57:02 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -286,8 +286,7 @@ if user_input_excel is not None:
                                         name_show = df.iloc[random.randint(1, len(df)) - 1]
                                         col_Z, col_ZZ, col_ZZZ = st.columns([1,20,1])
                                         with col_ZZ:
-                                            with st.container():
-                                                st.markdown(f"<h1 style='text-align: center;'>{str(name_show['Nama Lengkap'])}</h1>", unsafe_allow_html=True)
+                                            st.markdown(f"<h1 style='text-align: center;'><br>{str(name_show['Nama Lengkap'])}</h1>", unsafe_allow_html=True)
                                     time.sleep(0.1)
                                     t -= 1
                                     st.empty()
