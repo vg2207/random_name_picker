649caad29279fee1b0b3aeffd0dc1cee6bcf249c
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-07-24 13:55:03 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -498,19 +498,19 @@ if user_input_excel is not None:
                         t10 -= 1
                         st.empty()
                 
-                st.write(substitute_winners_name_all)
+                # st.write(substitute_winners_name_all)
                 row_number_to_show_substitute = st.session_state.count_2 - 1
                 
-                st.write(data_to_show_substitute.iloc[row_number_to_show_substitute,].reset_index(drop=True))
-                # data_to_show_substitute = pd.DataFrame(substitute_winners_name_all[row_number_to_show_substitute]).reset_index(drop=True)
-                # # st.write(substitute_winners_name_all[row_number_to_show_substitute])
+                
+                data_to_show_substitute = substitute_winners_name_all.loc[row_number_to_show_substitute]
+
                 # data_to_show_substitute.index += 1
                 
                 # st.write(data_to_show_substitute)
                 
 
-                # with st.container():
-                #     st.dataframe(data_to_show_substitute[["NIK", "Nama Lengkap","Company"]], use_container_width=True)
+                with st.container():
+                    st.dataframe(data_to_show_substitute[["NIK", "Nama Lengkap","Company"]], use_container_width=True)
 
               
 
