6d79029b1365acc6b636da6bac0beb7e01ff3c7b
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-07-24 12:20:48 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -414,10 +414,10 @@ if user_input_excel is not None:
         
         try :
             df_remain_all = st.session_state.df_remaining_participant.copy().reset_index(drop=True)
-            st.write(df_remain_all)
+            # st.write(df_remain_all)
 
             df_remain = df_remain_all.loc[df_remain_all['Company'] == 'LTL'].reset_index(drop=True)
-            st.write(df_remain)
+            # st.write(df_remain)
             
             random.seed(st.session_state.user_input_seed)
             substitute_winners_name_all = df_remain.sample(frac=1, random_state=st.session_state.user_input_seed).reset_index(drop=True)
@@ -457,9 +457,9 @@ if user_input_excel is not None:
                         color: white;
                     }""",
                 ):
-                    button_clicked_20 = st.button("Start 2", type="primary", use_container_width=True, on_click=increment_counter_2)
+                    button_clicked_20 = st.button("Start ", type="primary", use_container_width=True, on_click=increment_counter_2)
             with col_21 :
-                button_clicked_21 = st.button("Clear 2", type="secondary", use_container_width=True)
+                button_clicked_21 = st.button("Clear ", type="secondary", use_container_width=True)
             with col_22 :
                 with stylable_container(
                     "grey",
@@ -469,50 +469,50 @@ if user_input_excel is not None:
                         color: white;
                     }""",
                 ):
-                    button_clicked_22 = st.button("Reset 2", type="secondary", use_container_width=True, on_click=reset_counter_2)
+                    button_clicked_22 = st.button("Reset ", type="secondary", use_container_width=True, on_click=reset_counter_2)
             
             
 
-        #     if button_clicked_20: 
+            if button_clicked_20: 
     
-        #         if st.session_state.count1 <= len(df_remain) :                    
+                if st.session_state.count1 <= len(substitute_winners_name_all) :                    
                     
-        #             st.markdown("""
-        #             <style>
-        #             .big-font {
-        #                 font-size:200px !important;
-        #                 text-align: center;
-        #                 }
-        #             </style>
-        #             """, unsafe_allow_html=True)
-        #             t10 = 5
-        #             with st.empty():
-        #                 while t10:
-        #                     mins, secs = divmod(t10, 60)
-        #                     timer = t10
-        #                     with st.container():
-        #                         st.markdown(f'<p class="big-font">{timer}</p>', unsafe_allow_html=True)
-        #                     time.sleep(1)
-        #                     t10 -= 1
-        #                     st.empty()
+                    st.markdown("""
+                    <style>
+                    .big-font {
+                        font-size:200px !important;
+                        text-align: center;
+                        }
+                    </style>
+                    """, unsafe_allow_html=True)
+                    t10 = 5
+                    with st.empty():
+                        while t10:
+                            mins, secs = divmod(t10, 60)
+                            timer = t10
+                            with st.container():
+                                st.markdown(f'<p class="big-font">{timer}</p>', unsafe_allow_html=True)
+                            time.sleep(1)
+                            t10 -= 1
+                            st.empty()
                     
     
-        #             row_number_to_show = st.session_state.count1 - 1
-        #             data_to_show = substitute_winners_name_all[row_number_to_show].reset_index(drop=True)
-        #             data_to_show.index += 1
+                    row_number_to_show_substitute = st.session_state.count1 - 1
+                    data_to_show_substitute = substitute_winners_name_all[row_number_to_show_substitute].reset_index(drop=True)
+                    data_to_show_substitute.index += 1
 
-        #             with st.container():
-        #                 st.dataframe(data_to_show[["NIK", "Nama Lengkap","Company"]], use_container_width=True)
+                    with st.container():
+                        st.dataframe(data_to_show_substitute[["NIK", "Nama Lengkap","Company"]], use_container_width=True)
     
                   
     
-        #         else :
-        #             with st.empty():
-        #                 st.markdown(f"<h1 style='text-align: center;'>CONGRATS TO ALL THE WINNERS!</h1>", unsafe_allow_html=True)
+                else :
+                    with st.empty():
+                        st.markdown(f"<h1 style='text-align: center;'>CONGRATS TO ALL THE WINNERS!</h1>", unsafe_allow_html=True)
     
             
-        #         # st.write('Count = ', st.session_state.count)
-        #         st.balloons()
+                # st.write('Count = ', st.session_state.count)
+                st.balloons()
         except :
             st.error('You need to submit the number of winners and prize')
 
