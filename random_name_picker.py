44a6aec3d2965b1b7db0f27539970e5214e83cbf
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-07-24 10:05:36 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -288,7 +288,7 @@ if user_input_excel is not None:
                                         col_Z, col_ZZ, col_ZZZ = st.columns([1,20,1])
                                         with col_ZZ:
                                             st.markdown(f"<h1 style='text-align: center;'><br>{str(name_show['Nama Lengkap'])}</h1>", unsafe_allow_html=True)
-                                    time.sleep(0.1)
+                                    time.sleep(0.15)
                                     t -= 1
                                     st.empty()
                         
