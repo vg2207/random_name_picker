94f2161f3be882adc02ef83946ddacfeac807226
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-11-11 18:28:17 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -335,7 +335,17 @@ if user_input_excel is not None:
                         m=0
                         for col in row1:
                             tile = col.container()
-                            tile.markdown(f"<h3 style='text-align: center;'>{str(pd.DataFrame(data_to_show).loc[st.session_state.count*(m+1),'ID Karyawan'])}<br>{str(pd.DataFrame(data_to_show).loc[st.session_state.count*(m+1),'Nama Lengkap'])}<br>{str(pd.DataFrame(data_to_show).loc[st.session_state.count*(m+1),'Company'])}</h3>", unsafe_allow_html=True)
+                            with stylable_container(
+                                "border",
+                                css_styles="""
+                                .container1 {
+                                border: 2px solid #3498db;
+                                border-radius: 8px;
+                                padding: 10px;
+                                margin-bottom: 20px;
+                                }""",
+                            ):
+                                tile.markdown(f"<h3 style='text-align: center;'>{str(pd.DataFrame(data_to_show).loc[st.session_state.count*(m+1),'ID Karyawan'])}<br>{str(pd.DataFrame(data_to_show).loc[st.session_state.count*(m+1),'Nama Lengkap'])}<br>{str(pd.DataFrame(data_to_show).loc[st.session_state.count*(m+1),'Company'])}</h3>", unsafe_allow_html=True)
                             m+=1
 
                     # elif st.session_state.count <= 4 :
