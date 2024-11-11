38c9ca5d8a3f63bcabcbb4e38e1962f53f706c0b
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-11-11 11:06:55 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -27,14 +27,14 @@ st.set_page_config(layout="wide")
 
 col_A, col_B, col_C = st.columns([1, 3, 1])
 
-with col_A :
+with col_C :
     logo = Image.open('assets_logo/Logo Lautan Luas.png')
     st.image(logo)
 with col_B:
     with st.container():
         st.markdown(f"<h1 style='text-align: center;'>Undian Acara Natal<br>PT Lautan Luas Tbk.</h1>", unsafe_allow_html=True)
-with col_C :
-    logo_a = Image.open('assets_logo/anniversary_logo.png')
+with col_A :
+    logo_a = Image.open('assets_logo/christmas_logo.png')
     st.image(logo_a)
         
 # logo = Image.open('assets_logo/LOGO_FIXED.png')
