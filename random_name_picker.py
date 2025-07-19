1b36834ad36cac69ea6bd1920c09da6c4d764121
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2025-07-19 17:21:36 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -242,6 +242,8 @@ if user_input_excel is not None:
                         winners_data.index = winners_data.index + 1
                         print(winners_data)
                         winners = winners_data.to_excel(writer, sheet_name=sheetname)
+            except :
+                st.error()
             
                     
 
