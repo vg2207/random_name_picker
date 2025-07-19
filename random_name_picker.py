19a1e0f90177b4d385a9e858136c0936a21b629b
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2025-07-19 16:58:21 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -173,15 +173,13 @@ if user_input_excel is not None:
             
             random.seed(st.session_state.user_input_seed)
             df_ltl_only_winner_row = random.sample(range(len(df_ltl_only)), 41)
-            st.write(df_remaining_non_ltl)
             df_ltl_only_winner = df_ltl_only.iloc[df_ltl_only_winner_row].reset_index(drop=True)
-            st.write(df_ltl_only_winner)
+            # st.write(df_ltl_only_winner)
             df_remaining_ltl = df_ltl_only.drop(df_ltl_only_winner_row).reset_index(drop=True)
-            st.write(df_remaining_ltl)
+            # st.write(df_remaining_ltl)
     
             # random.seed(st.session_state.user_input_seed)
-            df_remaining_participant = pd.concat([df_remaining_ltl, df_remaining_non_ltl], axis=0)
-            # df_remaining_participant = pd.concat([df_remaining_ltl, df_remaining_non_ltl], axis=0).sample(frac=1, random_state=st.session_state.user_input_seed).reset_index(drop=True)
+            df_remaining_participant = pd.concat([df_remaining_ltl, df_remaining_non_ltl], axis=0).sample(frac=1, random_state=st.session_state.user_input_seed).reset_index(drop=True)
             st.write(df_remaining_participant)
     
             random.seed(st.session_state.user_input_seed)
