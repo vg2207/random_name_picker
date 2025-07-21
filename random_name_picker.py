b89648904bf86ef769e692de6a3cf4757d15b397
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2025-07-21 11:13:22 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -181,7 +181,7 @@ if user_input_excel is not None:
         # st.write(df_remaining_non_ltl)
         
         random.seed(st.session_state.user_input_seed)
-        df_ltl_only_winner_row = random.sample(range(len(df_ltl_only)), 41)
+        df_ltl_only_winner_row = random.sample(range(len(df_ltl_only)), 63)
         df_ltl_only_winner = df_ltl_only.iloc[df_ltl_only_winner_row].reset_index(drop=True)
         # st.write(df_ltl_only_winner)
         df_remaining_ltl = df_ltl_only.drop(df_ltl_only_winner_row).reset_index(drop=True)
