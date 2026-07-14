109d944505539f7c7ca5eed516d792fb1b6a58ad
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2026-07-14 18:13:47 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -72,7 +72,7 @@ with tab1 :
 
 
         with stylable_container(
-                    key="blue_button",
+                    key='blue_button',
                     css_styles="""
                     button {
                         background-color: #0349b3;
