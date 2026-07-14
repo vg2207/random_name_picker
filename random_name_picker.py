539b1c956f5b364dda29536f0b3bf638e7550ec5
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2026-07-14 18:15:12 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -71,14 +71,14 @@ with tab1 :
 
 
 
-        with stylable_container(key="blue_button",
+            with stylable_container(key="blue_button",
                     css_styles="""
                     button {
                         background-color: #0349b3;
                         color: white;
                     }""",
                 ):
-            button_clicked_7 = st.button("Submit", type="primary", use_container_width=True)
+                button_clicked_7 = st.button("Submit", type="primary", use_container_width=True)
 
 
     new_row = pd.DataFrame(
