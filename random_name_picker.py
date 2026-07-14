152ca2a5dd82d10826a6b1fce1085721c2496350
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2026-07-14 16:54:26 +0700

    Update random_name_picker.py

--- random_name_picker.py
+++ random_name_picker.py
@@ -173,17 +173,17 @@ with tab2 :
     df_all_participant['Employment Type'] >= 'Permanent'].loc[
     df_all_participant['Employee Category'] != 'Director'].reset_index(drop=True)
 
-    st.write(df_for_grandprize)
+    # st.write(df_for_grandprize)
 
 
     random.seed(st.session_state.user_input_seed)
     df_grandprize_winner_row = random.sample(range(len(df_for_grandprize)), 4)
     df_grandprize_winner = df_for_grandprize.iloc[df_grandprize_winner_row].reset_index(drop=True)
 
-    st.write(df_grandprize_winner)
+    # st.write(df_grandprize_winner)
 
     df_remaining_participant = df_all_participant[~df_all_participant['Employee Id'].isin(df_grandprize_winner['Employee Id'])].loc[df_all_participant['Employee Category'] != 'Director'].reset_index(drop=True)
-    st.write(df_remaining_participant)
+    # st.write(df_remaining_participant)
 
     
 
